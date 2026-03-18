138ceb20332291212c0482633dc8f751b6879928
Gianmauro Cuccuru <gianmauro.cuccuru@fht.org>
2026-03-18 10:44:15 +0100

    fix: add the GWASLab compatible logger

--- src/gwaspipe/__init__.py
+++ src/gwaspipe/__init__.py
@@ -1,7 +1,7 @@
 import importlib.metadata
+import time
 
 from cloup import Context, HelpFormatter, HelpTheme, Style
-from gwaslab.info.g_Log import Log
 from loguru import logger as a_logger
 
 __all__ = ["__appname__", "__version__", "formatter_settings", "context_settings", "logger", "Log"]
@@ -25,3 +25,19 @@ context_settings = Context.settings(formatter_settings=formatter_settings)
 
 logger = a_logger
 logger.remove()
+
+
+# GWASLab compatible logger
+class Log:
+    def __init__(self, path):
+        self.path = path
+
+    def write(self, *message, end="\n", show_time=True, verbose=True):
+        timestamp = str(time.strftime("%Y/%m/%d %H:%M:%S")) if show_time else ""
+        line = (timestamp + " " if show_time else "") + " ".join(map(str, message)) + end
+
+        if verbose:
+            print(line, end="")
+
+        with open(self.path, "a") as f:
+            f.write(line)
